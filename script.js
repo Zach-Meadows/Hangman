ea24e71927f07d04619f69056405405015015fcf
Zach Meadows <zmeadows1@gmail.com>
2019-08-27 19:52:09 -0400

    added images and flexbox to style

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('js is linked!')
+//declaring variable to use throught the program
+let input = document.querySelector("input")
+
+input.addEventListener("keypress", function(evt){
+    if (evt.keyCode === 13) {
+        let wordArray = [];
+        let word = input.value;
+        for (i = 0; i < word.length; i++) {
+            wordArray.push(word[i])
+        }
+        console.log(wordArray);
+    }
+})
