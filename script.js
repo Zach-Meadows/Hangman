7b927bde0bd7a17578d3a11471bab38af600d3c5
Zach Meadows <zmeadows1@gmail.com>
2019-08-27 20:12:01 -0400

    add event listeners to input and submit, added comments

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-//declaring variable to use throught the program
-let input = document.querySelector("input")
+//declaring variables to use throught the program
 
+//find input for word being entered
+let input = document.querySelector("input")
+//find submit button
+let submit = document.querySelector(".submit")
+
+//add event listener to input for enter key
 input.addEventListener("keypress", function(evt){
     if (evt.keyCode === 13) {
-        let wordArray = [];
-        let word = input.value;
-        for (i = 0; i < word.length; i++) {
-            wordArray.push(word[i])
-        }
-        console.log(wordArray);
+        //prevent default, and click submit button
+        evt.preventDefault();
+       submit.click();
     }
+})
+
+//add event listener for submit button
+submit.addEventListener("click", function(){
+    //declare empty array to put word pieces into. 
+    let wordArray = [];
+    //assign input word to a variable
+    let word = input.value;
+    //loop through the word putting each letter in to an array seperately
+    for (i = 0; i < word.length; i++) {
+        wordArray.push(word[i])
+    }
+    //print the array
+    console.log(wordArray);
 })
