00a78472299a3aefc3fbd380a5f5849261b14f73
Zach Meadows <zmeadows1@gmail.com>
2019-08-28 11:45:47 -0400

    added fill blank functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let input = document.querySelector("input")
 //find submit button
 let submit = document.querySelector(".submit")
-// define keys to be produced for visual keyboard, generate keyboard
+// define keys to be produced for visual keyboard, generate keyboard, with event listener
 var alphabet = [] 
 function genCharArray(charA, charZ) {
     a = charA.charCodeAt(0);
@@ -15,6 +15,21 @@ function genCharArray(charA, charZ) {
         let letter = document.createTextNode(`${capital}`)
         key.appendChild(letter);
         key.className = `key ${String.fromCharCode(a)}`
+        //add event listener to each key
+        key.addEventListener('click', function(evt){
+            evt.target.style.backgroundColor = 'red';
+            checkLetter = evt.target.innerText
+            if (wordArray.includes(checkLetter)) {
+                wordArray.forEach(function(element, i) {
+                    console.log(i)
+                    if (element === checkLetter) {
+                        document.querySelector(`.blank${i + 1}`).innerText = element
+                    }
+                });
+            } else {
+                console.log('none of that letter')
+            }
+        })
         document.querySelector('.visualKeyboard').appendChild(key)
     }
 }
@@ -28,17 +43,19 @@ input.addEventListener("keypress", function(evt){
        submit.click();
     }
 })
-
+let wordArray = [];
 //add event listener for submit button
 submit.addEventListener("click", function(){
     //declare empty array to put word pieces into. 
-    let wordArray = [];
+   
     //assign input word to a variable
     let word = input.value;
     document.querySelector('.wordChoice').style.display = 'none';
+    document.querySelector('.visualKeyboard').style.display = 'grid';
     //loop through the word putting each letter in to an array seperately
     for (i = 0; i < word.length; i++) {
-        wordArray.push(word[i]);
+        let capital = word[i].toUpperCase()
+        wordArray.push(capital);
         let blank = document.createElement('P');
         let empty = document.createTextNode('__')
         blank.appendChild(empty);
@@ -47,5 +64,10 @@ submit.addEventListener("click", function(){
     }
     //print the array
     console.log(wordArray);
+    //add event listener to document to click visual keyboard on keypress.
+    document.body.addEventListener('keypress', function(evt){
+        evt.preventDefault()
+        document.querySelector(`.${evt.key}`).click()
+    })
 
 })
